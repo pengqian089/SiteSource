b66a017845aa5f7d49da36b152171b0ec2733764
pengqian089 <pengqian089@gmail.com>
2022-11-10 17:01:32 +0800

    添加cherry-markdown

---
+++ library/cherry-markdown/types/toolbars/hooks/CheckList.d.ts
@@ -0,0 +1,8 @@
+/**
+ * 插入检查项的按钮
+ */
+export default class CheckList extends MenuBase {
+    constructor($cherry: any);
+    $dealSelection(selection: any): any;
+}
+import MenuBase from "@/toolbars/MenuBase";
---
+++ library/cherry-markdown/types/toolbars/hooks/Insert.d.ts
@@ -0,0 +1,7 @@
+/**
+ * "插入"按钮
+ */
+export default class Insert extends MenuBase {
+    constructor($cherry: any);
+}
+import MenuBase from "@/toolbars/MenuBase";
